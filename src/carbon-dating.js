9e6f60fc0f92dfef8a9cab69b8fbc6dba9b2aa69
Aleksey Osipov <alexey11rus@gmail.com>
2020-02-16 20:51:27 +0300

    fix: bug fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,12 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample( sampleActivity ) {
   throw 'Not implemented';
-  return typeof(sampleActivity) === "string" ?
-    Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/(0.693/HALF_LIFE_PERIOD)) : false;  
+  if ( typeof(sampleActivity) != "string") {
+    return false;
+  }
+  sampleActivity = Number(sampleActivity);
+  if ( sampleActivity <= 0 || sampleActivity > 15 ) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 };
