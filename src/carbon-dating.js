51670ea360df7e2ece519044730736cc88b1e66f
Aleksey Osipov <alexey11rus@gmail.com>
2020-02-16 22:44:30 +0300

    fix: carbon-dating bugfix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,14 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample( sampleActivity ) {
-  throw 'Not implemented';
-  if ( typeof(sampleActivity) != "string") {
+ module.exports =  function dateSample( sampleActivity ) {
+  if (typeof(sampleActivity) != "string") {
     return false;
   }
-  sampleActivity = Number(sampleActivity);
-  if ( sampleActivity <= 0 || sampleActivity > 15 ) {
+  sampleActivity = parseFloat(sampleActivity);
+  if ( (sampleActivity <= 0 || sampleActivity > 15) || isNaN(sampleActivity)  ) {
     return false;
   }
+  
   return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,4 @@
 module.exports = function countCats( matrix ) {
-  throw 'Not implemented';
   let totalCats = 0;
   matrix.forEach(el => {
     el.forEach(elem => {
