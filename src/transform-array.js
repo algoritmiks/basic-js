f946e59df71eacf14ac722ec787a3bb7ecbbc6c6
Aleksey Osipov <alexey11rus@gmail.com>
2020-02-17 23:02:33 +0300

    feat: transform-array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+    res = [];
+    for ( let elemIndex=0; elemIndex < arr.length; elemIndex++ ) {
+      switch(arr[elemIndex]){
+          case '--double-next':
+              if (elemIndex < arr.length-1) {
+                  res.push(arr[elemIndex+1]);
+              }
+              break;
+          case '--discard-prev':
+              res.pop();
+              break;
+          case '--discard-next':
+              if (elemIndex < arr.length-1) {
+                  elemIndex+=1;
+              }
+              break;
+          case '--double-prev':
+              if ( elemIndex > 0 ) {
+              res.push(arr[elemIndex-1]);
+              }
+              break;
+          default: 
+              res.push(arr[elemIndex]);
+          }
+      }
+      return res;
 };
