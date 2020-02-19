fd5cb9008d1546abb2f4bf9972282037e1f9c855
Aleksey Osipov <alexey11rus@gmail.com>
2020-02-19 13:55:57 +0300

    feat: extended-repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,42 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+    let result = '';
+    
+    if ( str ) {
+        str = str.toString();
+    }
+    
+    if ( options.additionSeparator === undefined ) {
+        options.additionSeparator = '|';
+    };
+
+    if ( options.separator === undefined ) {
+        options.separator = '+';
+    };
+
+    if ( options.addition === undefined ) {
+        options.addition = '';
+    };
+
+    if ( options.repeatTimes === undefined ) {
+        options.repeatTimes = 1;
+    };    
+    
+    if ( options.additionRepeatTimes === undefined ) {
+        options.additionRepeatTimes = 1;
+    };
+
+    for ( let i=0; i<options.repeatTimes; i++ ) {
+        result+=`${str}`;
+        for (let add=0; add<options.additionRepeatTimes; add++) {
+            result += `${options.addition}`;
+            if (options.additionRepeatTimes-1 !== add ) {
+                result += `${options.additionSeparator}`;
+            }
+        }
+        if (options.repeatTimes-1 !== i) {
+           result += `${options.separator}`;
+        }
+    }
+    return result;
 };
   
