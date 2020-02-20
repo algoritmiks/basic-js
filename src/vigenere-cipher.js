7edda70182533d2db5d707163ab8a28d59355ad4
Aleksey Osipov <alexey11rus@gmail.com>
2020-02-20 15:45:39 +0300

    feat: vigenere cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -33,9 +33,32 @@ class VigenereCipheringMachine {
         
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(str, key) {
+        if (!str || !key) {
+            throw new Error();
+        };
+        const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let currentIndex = 0;
+        let keyLength = key.length;
+        let decryptedLetter = '';
+        for(let i in str) {
+          if ( alfabet.indexOf(str[i])<0 ) {
+            decryptedLetter += str[i];
+            continue;
+          } 
+          let currentLetterIndexInAlfabet = alfabet.indexOf(str[i]);
+          let keyLetterIndexInAlfabet = alfabet.indexOf(key[currentIndex%keyLength]);
+          decryptedLetter = decryptedLetter + alfabet[(currentLetterIndexInAlfabet - keyLetterIndexInAlfabet + 26) % 26];
+          currentIndex += 1;
+        }
+
+        if (!this.direct) {
+            return decryptedLetter.split('').reverse().join('');
+        }
+        return decryptedLetter;
+        
     }
 }
 
