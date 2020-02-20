f981d9a06a9c873ea51539fa393f49fb44959950
Aleksey Osipov <alexey11rus@gmail.com>
2020-02-20 14:49:54 +0300

    feat vigenere cipher encrypt done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,7 +1,36 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor(direct = true) {
+        this.direct = direct;
+      }
+
+    encrypt(str, key) {
+
+        if (!str || !key) {
+            throw new Error();
+        };
+
+        const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let currentIndex = 0;
+        let keyLength = key.length;
+        let encryptedLetter = '';
+        for(let i in str) {
+          if ( alfabet.indexOf(str[i])<0 ) {
+            encryptedLetter += str[i];
+            continue;
+          } 
+          let currentLetterIndexInAlfabet = alfabet.indexOf(str[i]);
+          let keyLetterIndexInAlfabet = alfabet.indexOf(key[currentIndex%keyLength]);
+          encryptedLetter = encryptedLetter + alfabet[(currentLetterIndexInAlfabet + keyLetterIndexInAlfabet) % 26];
+          currentIndex += 1;
+        }
+        if (!this.direct) {
+            return encryptedLetter.split('').reverse().join('');
+        }
+        return encryptedLetter;
+        
     }
 
     decrypt() {
